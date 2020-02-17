96cc7e8bd2339bc09261e04e12377db391305faf
Nuo (Tony) Chen <chen2886@purdue.edu>
2020-02-17 18:25:50 -0500

    First Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Tony Chen on 2/17/20.
+//  Copyright © 2020 Tony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
